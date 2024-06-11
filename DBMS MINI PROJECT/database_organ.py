fa3864e2552f1bad1214b2d85199312244d07a02
Niyathi <220701189@rajalakshmi.edu.in>
2024-06-11 20:53:15 +0530

    Add files via upload

---
+++ DBMS MINI PROJECT/database_organ.py
@@ -0,0 +1,19 @@
+import mysql.connector
+import streamlit as st
+import pandas as pd 
+import plotly.express as px
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="swenik2112",
+    database="PES1UG20CS078_organ"
+)
+
+c = mydb.cursor()
+
+def view_organ_details():
+      c.execute('SELECT * FROM organ_available')
+      data=c.fetchall()
+      return data
